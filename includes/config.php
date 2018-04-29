6925c2071d3deb90cc557605731a2f1b7736607d
ruben <room3622@gmail.com>
2018-04-29 20:19:57 +0100

    Go

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,1 @@
-<?php
-
-// Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");//host
-defined('DB_USER')   ? null : define("DB_USER", "root");//username
-defined('DB_PASS')   ? null : define("DB_PASS", "cft.820+hFFZ");//pass
-defined('DB_NAME')   ? null : define("DB_NAME", "Server");//db name
-
-
-?>
+<?php// Database Constantsdefined('DB_SERVER') ? null : define("DB_SERVER", "localhost");//hostdefined('DB_USER')   ? null : define("DB_USER", "root");//usernamedefined('DB_PASS')   ? null : define("DB_PASS", "");//passdefined('DB_NAME')   ? null : define("DB_NAME", "");//db name?>
--- includes/initialize.php
+++ includes/initialize.php
@@ -9,16 +9,14 @@ require_once ('config.php');
 // load core objects
 
 require_once ('session.php');
-require_once ('Facebook/ini.php');
+//require_once ('Facebook/ini.php');
 
 require_once ('database.php');
 require_once ('database_object.php');
 
 require_once ('user.php');
-require_once ('functions.php');
-require_once ('inputs.php');
-require_once("forms.php");
-require_once("messages.php");
-require_once("token.php")
-
-?>
+//require_once ('functions.php');
+//require_once ('inputs.php');
+//require_once("forms.php");
+//require_once("messages.php");
+//require_once("token.php")
