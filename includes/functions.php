8816fbb9c4a7adfabefcfd5b8633e4c1f8752fc1
ruben <room3622@gmail.com>
2018-04-29 20:35:13 +0100

    Go

--- includes/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//if sesion is login give outo atribute to a object 
-// example echo  User->email;
-if($session->is_logged_in()) {
-$id = $_SESSION['user_id']; 
-$user = User::find_by_id($id);
-}
-?>
