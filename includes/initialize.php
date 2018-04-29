65823d9c7b387a9413eb4e6c178f51e014bcec1a
ruben <room3622@gmail.com>
2018-04-29 20:45:32 +0100

    Go

--- includes/initialize.php
+++ includes/initialize.php
@@ -9,14 +9,9 @@ require_once ('config.php');
 // load core objects
 
 require_once ('session.php');
-//require_once ('Facebook/ini.php');
+
 
 require_once ('database.php');
 require_once ('database_object.php');
 
 require_once ('user.php');
-//require_once ('functions.php');
-//require_once ('inputs.php');
-//require_once("forms.php");
-//require_once("messages.php");
-//require_once("token.php")
